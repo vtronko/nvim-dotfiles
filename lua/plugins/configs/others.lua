9389ca45bed5bb840be9bbe1cbb68dd688b99255
Vladyslav Tronko <git@vtronko.me>
2021-11-14 22:33:14 +0200

    fix autopairs

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -4,17 +4,15 @@ local config = require("core.utils").load_config()
 
 M.autopairs = function()
    local present1, autopairs = pcall(require, "nvim-autopairs")
-   local present2, autopairs_completion = pcall(require, "nvim-autopairs.completion.cmp")
+   local present2, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
 
    if not (present1 or present2) then
       return
    end
 
    autopairs.setup()
-   autopairs_completion.setup {
-      map_complete = true, -- insert () func completion
-      map_cr = true,
-   }
+   local cmp = require "cmp"
+   cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
 M.autosave = function()
